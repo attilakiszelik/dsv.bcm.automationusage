68039c8b90e82618db65ed9b0b44b2285c577090
A <attila.kiszelik@gmail.com>
2022-11-10 15:16:51 +0100

    start

---
+++ src/main/java/com/kaev/automationusage/AutomationusageApplication.java
@@ -0,0 +1,13 @@
+package com.kaev.automationusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationusageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationusageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaev/automationusage/AutomationusageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaev.automationusage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationusageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
