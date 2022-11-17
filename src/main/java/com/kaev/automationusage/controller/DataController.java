2245051988470c589d9a681d1f2618b848d479f8
A <attila.kiszelik@gmail.com>
2022-11-17 19:08:01 +0100

    connect to azure server

--- src/main/java/com/kaev/automationusage/controller/DataController.java
+++ src/main/java/com/kaev/automationusage/controller/DataController.java
@@ -1,6 +1,7 @@
 package com.kaev.automationusage.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaev.automationusage.service.DataService;
@@ -8,12 +9,13 @@ import com.kaev.automationusage.service.DataService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/automationusage")
 @RequiredArgsConstructor
 public class DataController {
 
 	private final DataService dataService;
 	
-	@GetMapping("/")
+	@GetMapping
 	public String getData() {
 		
 		return "";
--- src/main/java/com/kaev/automationusage/dto/DataDto.java
+++ src/main/java/com/kaev/automationusage/dto/DataDto.java
@@ -1,5 +1,25 @@
 package com.kaev.automationusage.dto;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DataDto {
 
+	@Id
+	@GeneratedValue
+	@EqualsAndHashCode.Include()
+	private long id;
+	
+	private String process;
+	private LocalDateTime timestamp;
+	private String username;
+	
 }
